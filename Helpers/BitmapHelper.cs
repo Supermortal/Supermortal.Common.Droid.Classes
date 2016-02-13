2ad3c0ef0772363b4bebd11eb0acc39d28076897
Christopher Pink <christopherbyronpink@yahoo.com>
2016-02-13 12:30:18 -0700

    Added BitmapHelper

---
+++ Helpers/BitmapHelper.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Android.Graphics;
+using Android.App;
+
+namespace Supermortal.Common.Droid.Helpers
+{
+    public static class BitmapHelper
+    {
+        public static Bitmap LoadAndResizeBitmap(string fileName, int width, int height)
+        {
+            // First we get the the dimensions of the file on disk
+            BitmapFactory.Options options = new BitmapFactory.Options { InJustDecodeBounds = true };
+            BitmapFactory.DecodeFile(fileName, options);
+
+            // Next we calculate the ratio that we need to resize the image by
+            // in order to fit the requested dimensions.
+            int outHeight = options.OutHeight;
+            int outWidth = options.OutWidth;
+            int inSampleSize = 1;
+
+            if (outHeight > height || outWidth > width)
+            {
+                inSampleSize = outWidth > outHeight
+                    ? outHeight / height
+                    : outWidth / width;
+            }
+
+            // Now we will load the image and have BitmapFactory resize it for us.
+            options.InSampleSize = inSampleSize;
+            options.InJustDecodeBounds = false;
+            Bitmap resizedBitmap = BitmapFactory.DecodeFile(fileName, options);
+
+            return resizedBitmap;
+        }
+
+        public static Bitmap LoadAndResizeBitmap(Android.Content.Res.Resources resources, int resId, int width, int height)
+        {
+            // First we get the the dimensions of the file on disk
+            BitmapFactory.Options options = new BitmapFactory.Options { InJustDecodeBounds = true };
+            BitmapFactory.DecodeResource(resources, resId, options);
+
+            // Next we calculate the ratio that we need to resize the image by
+            // in order to fit the requested dimensions.
+            int outHeight = options.OutHeight;
+            int outWidth = options.OutWidth;
+            int inSampleSize = 1;
+
+            if (outHeight > height || outWidth > width)
+            {
+                inSampleSize = outWidth > outHeight
+                    ? outHeight / height
+                    : outWidth / width;
+            }
+
+            // Now we will load the image and have BitmapFactory resize it for us.
+            options.InSampleSize = inSampleSize;
+            options.InJustDecodeBounds = false;
+            Bitmap resizedBitmap = BitmapFactory.DecodeResource(resources, resId, options);
+
+            return resizedBitmap;
+        }
+    }
+}
+
